f74e22fae8ace67a00186ab377b2e96a83a56552
Saurav Sahu <sauravsahu.12@live.com>
2018-04-08 21:10:07 +0530

    Setup database, debugging and use dotenv for env variables.
    Setup database, debugging and use dotenv for env variables.

--- src/server.js
+++ src/server.js
@@ -1,19 +1,29 @@
 import hapi from 'hapi';
 
+import './env';
+
 import routes from './routes';
+import models from './db/models';
 
 const server = new hapi.Server({
-  host: '0.0.0.0',
-  port: '8080',
+  host: process.env.HOST,
+  port: process.env.PORT,
 });
 
 server.route(routes);
 
+export default server;
+
 (async () => {
   try {
+    const sampleQuery = 'SELECT 1 + 1';
+    await models.sequelize.query(sampleQuery);
+    console.log(`Test db with sample query ${sampleQuery} succeeded.`);
+
     await server.start();
     console.log(`Server started at: ${server.info.uri}`);
-  } catch (err) {
-    throw err;
+  } catch (error) {
+    console.log(error);
+    process.exit();
   }
 })();
