500ceb846654d8748fbeb9c68c1ec50623015956
Saurav Sahu <sauravsahu.12@live.com>
2018-04-21 20:48:19 +0530

    add new trips route
    add new trips route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import ping from './ping';
 import users from './users';
 import friends from './friends';
+import trips from './trips';
 
 export default [
   ...ping,
   ...users,
   ...friends,
+  ...trips,
 ];
---
+++ src/routes/trips.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    path: '/api/trips',
+    method: 'POST',
+    handler: () => 'trips',
+  },
+];
+
