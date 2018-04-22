65a70b9de6239a60123ade176da4f7795b62cc5a
Saurav Sahu <sauravsahu.12@live.com>
2018-04-22 10:18:11 +0530

    implement trips api and fix association between trips and users.
    implement trips api and fix association between trips and users.

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -1,8 +1,43 @@
+import models from './../db/models';
+
 export default [
   {
     path: '/api/trips',
     method: 'POST',
-    handler: () => 'trips',
+    handler: async (request) => {
+      const {
+        name, description, createdByUserId, memberUserIds,
+      } = request.payload;
+
+      try {
+        // TODO: get createdByUserId from auth
+        const newTrip = {
+          name,
+          description,
+          createdByUserId,
+        };
+
+        const newTripEntity = await models.trips.create(newTrip);
+        await Promise.all(memberUserIds.map(userId => models.tripMembers.create({
+          userId,
+          tripId: newTripEntity.id,
+        })));
+
+        const data = await models.trips.findById(
+          newTripEntity.id,
+          {
+            include: [
+              {
+                model: models.users,
+                as: 'members',
+                through: 'tripMembers',
+              },
+            ],
+          },
+        );
+        return data;
+      } catch (error) { console.log(error.message); throw error; }
+    },
   },
 ];
 
