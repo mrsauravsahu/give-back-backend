a75b05816f1b22ddbc21b1d52ff8f3759bd2e203
Saurav Sahu <sauravsahu.12@live.com>
2018-04-26 11:44:56 +0530

    add updated graphql schema. return nested objects in response
    add updated graphql schema. return nested objects in response

--- src/resolvers/me.js
+++ src/resolvers/me.js
@@ -2,11 +2,9 @@ import models from '../db/models';
 
 export const me = id => models.users.find({
   where: { id },
-  attributes: ['id', 'createdAt', 'updatedAt'],
   include: [
     {
       model: models.facebooks,
-      attributes: ['firstName', 'lastName', 'pictureUrl', 'fbId'],
     },
   ],
 });
