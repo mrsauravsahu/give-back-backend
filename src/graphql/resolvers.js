0a50250b1a7e546903348d4ea879b9b92587afae
Sahu, S <sauravsahu.12@live.com>
2018-05-10 17:59:42 +0530

    Add scalar type for date. Return UTC format

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,3 +1,5 @@
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
 import allResolvers from './resolvers/index';
 
 export const resolvers = () => ({
@@ -6,6 +8,20 @@ export const resolvers = () => ({
     friends: () => allResolvers.friends(1),
     trips: () => allResolvers.trips(1),
   },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue: value => new Date(value),
+    serialize: value => value,
+    parseLiteral: (ast) => {
+      switch (ast.kind) {
+        case Kind.INT:
+          return parseInt(ast.value, 10);
+        default:
+          return null;
+      }
+    },
+  }),
 });
 
 export default resolvers;
